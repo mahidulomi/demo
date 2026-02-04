f9ec500b6820a039d49fd473ed1141a4165d6480
mahidulomi <2405055@ugrad.cse.buet.ac.bd>
2026-02-05 01:47:10 +0600

    update

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -11,9 +11,15 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+        // Match the FXML design size so it doesn't start tiny.
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 700);
+
         stage.setTitle("My First JavaFX App");
+        stage.setMinWidth(900);
+        stage.setMinHeight(650);
         stage.setScene(scene);
+        stage.centerOnScreen();
         stage.show();
     }
 
